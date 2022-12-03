f182dee6f5b983ae758b720cf48e9461f5634c86
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2022-12-02 17:06:26 -0800

    WIP: air-monitor-plots-class.js

--- PLAY/air-monitor-plots-class.js
+++ PLAY/air-monitor-plots-class.js
@@ -1,5 +1,7 @@
 // Plotting functions for air-monitor data.
 
+// https://github.com/pnwairfire/airfire-smoke-maps/blob/master/src/scripts/graphs.js
+
 function monitor_timeseriesPlot(figureID, monitor, id) {
 
   id = Math.floor(Math.random() * monitor.meta.numRows());
@@ -24,60 +26,163 @@ function monitor_timeseriesPlot(figureID, monitor, id) {
 
   const chart = Highcharts.chart(figureID, {
       chart: {
-          zoomType: 'x'
+        animation: false
+      },
+      plotOptions: {
+        series: {
+          animation: false
+        },
+        scatter: {
+          animation: false,
+          marker: { radius: 2.5, symbol: 'circle', fillColor: '#bbbbbb'}
+        }
       },
       title: {
-          text: title
-      },
-      subtitle: {
-          text: document.ontouchstart === undefined ?
-              'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+        text: title
       },
       xAxis: {
-          type: 'datetime'
+        type: 'datetime'
       },
       yAxis: {
-          title: {
-              text: 'PM2.5 ug/m3'
-          }
+        title: {
+          text: 'PM2.5 ug/m3'
+        }
       },
       legend: {
-          enabled: false
+        enabled: false
       },
-      // plotOptions: {
-      //     area: {
-      //         fillColor: {
-      //             linearGradient: {
-      //                 x1: 0,
-      //                 y1: 0,
-      //                 x2: 0,
-      //                 y2: 1
-      //             },
-      //             stops: [
-      //                 [0, Highcharts.getOptions().colors[0]],
-      //                 [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-      //             ]
-      //         },
-      //         marker: {
-      //             radius: 2
-      //         },
-      //         lineWidth: 1,
-      //         states: {
-      //             hover: {
-      //                 lineWidth: 1
-      //             }
-      //         },
-      //         threshold: null
-      //     }
-      // },
-
       series: [{
-          type: 'area',
           name: 'PM2.5',
+          type: 'scatter',
           pointInterval: 3600 * 1000,
           pointStart: startTime.valueOf(),
           data: data
       }]
   });
 
+};
+
+// -----------------------------------------------------------------------------
+
+// https://github.com/pnwairfire/airfire-smoke-maps/blob/master/src/scripts/graphs.js
+
+// export function pm25PopupGraph(uniqueID, monitor, seriesData) {
+//   Highcharts.setOptions({global: { useUTC: false } });
+//   $('#pm25GraphContainer' + uniqueID).highcharts({
+
+
+function monitor_pm25Plot(figureID, monitor, id) {
+
+  id = Math.floor(Math.random() * monitor.meta.numRows());
+
+  let ids = monitor.meta.array('deviceDeploymentID');
+
+  let startTime = monitor.data.array('datetime')[0];
+  let data = null;
+  let title = null;
+
+  if ( Number.isInteger(id) ) {
+    data = monitor.data.array(ids[id]);
+    title = monitor.meta.array('locationName')[id];
+  } else {
+    data = monitor.data.array(id);
+    title = 
+      monitor.meta
+      .params({id: id})
+      .filter((d, $) => d.deviceDeploymentID == $.id)
+      .array('locationName');
+  }
+
+  const chart = Highcharts.chart(figureID, {
+    chart: {
+      type: 'spline',
+      animation: false,
+      backgroundColor:'',
+      zoomType: 'x',
+      resetZoomButton: {
+          theme: {
+              fill: 'white',
+              stroke: 'silver',
+              r: 0,
+              states: {
+                  hover: {
+                      fill: '#41739D',
+                      style: {
+                          color: 'white'
+                      }
+                  }
+              }
+          },
+          position: {
+              align: 'left', // by default
+              verticalAlign: 'top', // by default
+              x: 0,
+              y: -10
+          }
+      }
+    },
+    title: {
+        text:   null,
+        align: 'left',
+        style: {
+            color: 'rgba(0,0,0,0.6)',
+            fontSize: 'small',
+            fontWeight: 'bold',
+            fontFamily: 'Open Sans, sans-serif'
+        }
+        //text: null
+    },
+    credits: {
+        enabled: false
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                enabled: false
+            },
+            color: "#525252"
+        }
+    },
+    xAxis: {
+        type: 'datetime',
+        tickColor: '#4e4e4e',
+        lineColor: '#4e4e4e',
+        tickAmount: 10,
+        tickWidth: 1,
+        labels: {
+            formatter: function () {
+                return Highcharts.dateFormat('%b %d', this.value);
+            },
+            //rotation: -90,
+            align: 'center'
+        }
+    },
+    yAxis: {
+        gridLineWidth: 0,
+        title: {
+            text: i18next.t('charts.hourly-pm'),
+            style: {
+                color: '#4e4e4e'
+            }
+        },
+        labels: {
+            format: '{value}',
+            style: {
+                color: '#4e4e4e'
+            }
+        }
+    },
+    series: [{
+        showInLegend: false,
+        pointInterval: 3600 * 1000,
+        pointStart: startTime.valueOf(),
+        // data: seriesData,
+        type: "spline",
+        dashStyle: 'shortdot',
+        tooltip: {
+            pointFormat: "PM2.5: {point.y}µg/m3",
+            xDateFormat: '%b %d %Y at %l%p'
+        }
+    }]
+  });
 };
